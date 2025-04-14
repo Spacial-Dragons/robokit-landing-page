c130b4667e5db2560ab27e700ba4619470327a42
Edgar Vergara <edgarvm13@hotmail.com>
2025-04-14 14:51:25 +0200

    feat: refactor Navbar component to include Link for navigation

--- src/app/(landing)/ui/Navbar.tsx
+++ src/app/(landing)/ui/Navbar.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export const Navbar = () => {
   return (
     <div className="sticky top-0 bg-black/80 z-20 w-full flex h-16 px-6 items-center border-b border-b-white/20 backdrop-blur-sm backdrop-saturate-150">
-      RoboKit
+      <div className="max-w-7xl mx-auto w-full px-5">
+        <Link href="/">RoboKit</Link>
+      </div>
     </div>
   );
 };
